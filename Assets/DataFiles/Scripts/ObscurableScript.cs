06e60dacdbb608ea2bd60cbef56c50197112ad5e
npyalex <43121843+npyalex@users.noreply.github.com>
2019-12-10 12:02:55 -0500

    12-10-2019
    Build window viewer

---
+++ Assets/DataFiles/Scripts/ObscurableScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObscurableScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+        {
+            r.material.renderQueue = 2002;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
