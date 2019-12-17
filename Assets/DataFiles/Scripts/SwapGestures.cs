3604c001e7dc10863d31d53d3a70f841ee358dd2
npyalex <43121843+npyalex@users.noreply.github.com>
2019-12-17 11:36:20 -0500

    12-17-2019
    incl volumetric video tests

---
+++ Assets/DataFiles/Scripts/SwapGestures.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapGestures : MonoBehaviour
+{
+    public GameObject objectOne, objectTwo;
+    private bool go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        go = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go == false)
+        {
+            objectOne.SetActive(true);
+            objectTwo.SetActive(false);
+        }
+        if (go == true)
+        {
+            objectOne.SetActive(false);
+            objectTwo.SetActive(true);
+        }
+    }
+
+    public void Swap()
+    {
+        go = !go;
+    }
+}
